40748a2058b1458e7952153e6128224688ec9184
evx001 <eviexeris@gmail.com>
2014-10-02 18:43:38 -0400

    predictionary

--- 000Play.playground/section-1.swift
+++ 000Play.playground/section-1.swift
@@ -8,6 +8,7 @@ import UIKit
 // first we name our class 
 
 //1
+//////////////////////////////////////////////////////////////////
 class TipCalc {
 //2 declaring properties on the class
 // and it's nice to give them intial values
@@ -26,7 +27,38 @@ class TipCalc {
         self.taxPct = taxPct // and here as well.
         subTotal = total / (taxPct + 1) // subtotal has no conflict
     }
+// 4 
+    func calcTipWithTipPct(tipPct:Double) -> Double {
+        return subTotal * tipPct
+    }
+
+// Dictionary here
+
+//////////////////////////////////////////////////////
+// 5
+//    func printPossibleTips() {
+//        let possibleTipsInferred = [0.15,0.18,0.20]
+//        let possibleTipsExplicit:[Double] = [0.15,0.18,0.20]
+//        for possibleTip in possibleTipsInferred {
+//            println("\(possibleTip*100)%: \calcTipWithTipPct(possible))")
+//        }
+
+//  what was prior to refactoring the above
+//        println("15%: \(calcTipWithTipPct(0.15))")
+//        println("18%: \(calcTipWithTipPct(0.18))")
+//        println("20%: \(calcTipWithTipPct(0.20))")
+/////////////////////////////////////////////////////
+// Note: calling a method on a class instance all but the first must be named
+/////////////////////////////////////////////////////
 
 }
+// end of object /////////////////////////////////
 
+let tipCalc = TipCalc (total: 33.25, taxPct: 0.06)
+tipCalc.printPossibleTips()
+
+// output
+//> "15%: 4.70518867924528"
+//> "18%: 5.64622641509434"
+//> "20%: 6.27358490566038"
 
