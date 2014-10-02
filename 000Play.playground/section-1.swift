1566e767419718d98699c26921d3f8c9237e2a17
evx001 <eviexeris@gmail.com>
2014-10-02 17:10:45 -0400

    rayWenderlich

--- 000Play.playground/section-1.swift
+++ 000Play.playground/section-1.swift
@@ -1,5 +1,21 @@
-// Playground - noun: a place where people can play
-
 import UIKit
+// according to Wenderlich
 
-var str = "Hello, playground"
+// LET OVER VAR!! for compiler optimization
+
+// Infer types whenever possible // compiler is smarter than you, mostly.
+
+// Double over Float cause if you need precision might as well be really precise
+
+
+
+// var Ints
+var tutorialTeam = 56
+var editorialTeam = 23
+var totalTeam = tutorialTeam + editorialTeam
+    // totalTeam += 1
+
+let priceInferred = 19.99
+let priceExplicit: Double = 19.99
+
+
