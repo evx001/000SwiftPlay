c2707f78564b8608d72379f00cc458cd07a8515f
evx001 <eviexeris@gmail.com>
2014-10-02 18:05:06 -0400

    tipCalc Object

--- 000Play.playground/section-1.swift
+++ 000Play.playground/section-1.swift
@@ -1,46 +1,33 @@
 import UIKit
-// according to Wenderlich
 
-// LET OVER VAR!! for compiler optimization
+// classes and methods
+    // subclassing is optional unlike objective-C
+        // class TipCalc : NSObject
 
-// Infer types whenever possible // compiler is smarter than you, mostly.
+// tips are configured preTax
+// first we name our class 
 
-// Double over Float cause if you need precision might as well be really precise
+//1
+class TipCalc {
+//2 declaring properties on the class
+// and it's nice to give them intial values
+//                              lest you declare them as options
+    let total: Double
+    let taxPct: Double
+    let subTotal: Double
 
+//3 we initialize with two properties 
+    // init is the keyword 
+    // you can have more than one
+    // but like records in Haskell
+    // the parameters // fields must have unique names
+    init(total:Double, taxPct:Double) {
+        self.total = total
+        self.taxPct = taxPct
+        subTotal = total / (taxPct + 1)
 
-////////////// var and let ///////////////
+    }
 
-var tutorialTeam = 56
-var editorialTeam = 23
-var totalTeam = tutorialTeam + editorialTeam
-    // totalTeam += 1
-
-/////// Double over Float
-let priceInferred = 19.99
-let priceExplicit: Double = 19.99
-
-//// Bools ////
-let onSaleInferred = true
-let onSaleExplicit: Bool = false
-
-///// Strings ////
-let nameInferred = "Winnie the Poo"
-let nameExplicit = "Winnie the Poo"
-
-///// if and string interp ////
-if onSaleInferred {
-    println("\(nameInferred) on sale for \(priceInferred)!")
-}else {
-    println("\(nameInferred) at regular price: \(priceInferred)!")
 }
-// to output any expression as a string ....
-// \(MyExpressionsResult) 
-
-print("\(totalTeam)")
-
-
-// -> view -> assistant editor -> console output from playground
-// also [command + enter] -> main view playground
-// also [option + command + enter] -> console 
 
 
