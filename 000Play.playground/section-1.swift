dfd8008bb5910eff66a007c789161108f3c44685
evx001 <eviexeris@gmail.com>
2014-10-02 17:26:29 -0400

    string interp

--- 000Play.playground/section-1.swift
+++ 000Play.playground/section-1.swift
@@ -8,14 +8,33 @@ import UIKit
 // Double over Float cause if you need precision might as well be really precise
 
 
+////////////// var and let ///////////////
 
-// var Ints
 var tutorialTeam = 56
 var editorialTeam = 23
 var totalTeam = tutorialTeam + editorialTeam
     // totalTeam += 1
 
+/////// Double over Float
 let priceInferred = 19.99
 let priceExplicit: Double = 19.99
 
+//// Bools ////
+let onSaleInferred = true
+let onSaleExplicit: Bool = false
+
+///// Strings ////
+let nameInferred = "Winnie the Poo"
+let nameExplicit = "Winnie the Poo"
+
+///// if and string interp ////
+if onSaleInferred {
+    println("\(nameInferred) on sale for \(priceInferred)!")
+}else {
+    println("\(nameInferred) at regular price: \(priceInferred)!")
+}
+// to output any expression as a string ....
+// \(MyExpressionsResult) 
+
+print("\(totalTeam)")
 
